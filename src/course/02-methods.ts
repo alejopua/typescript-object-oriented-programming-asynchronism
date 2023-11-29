fc7fa76c6929b4a1c60d470e9d29fb0e8ce01325
alejopua <alejopua@gmail.com>
2023-11-29 11:18:21 -0500

    [add](class): methods in class

---
+++ src/course/02-methods.ts
@@ -0,0 +1,43 @@
+export class myDate {
+    year: number;
+    month: number;
+    day: number;
+
+    constructor(year: number, month: number, day: number) {
+        this.year = year;
+        this.month = month;
+        this.day = day;
+    }
+
+    format(): string {
+        return `${this.year}/${this.month}/${this.day}`
+    }
+
+    add(amount: number, type: 'year' | 'month' | 'day') {
+        if(type === 'year') {
+            this.year += amount;
+        } else if (type === 'month') {
+            if (this.month >= 12) {
+                this.year += amount;
+                this.month = 1;
+            } else {
+                this.month += amount;
+            }
+        } else if (type === 'day') {
+            this.day += amount;
+        }
+    }
+}
+
+const firstDate = new myDate(2023, 12, 29);
+console.log('First date : ', {firstDate});
+console.log('First date format: ', firstDate.format());
+
+firstDate.add(1, 'year');
+console.log('First date add year: ', firstDate.format());
+
+firstDate.add(1, 'month');
+console.log('First date add month: ', firstDate.format());
+
+firstDate.add(1, 'day');
+console.log('First date add day: ', firstDate.format());
