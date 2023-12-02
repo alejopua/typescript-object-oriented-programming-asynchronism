cd25cca615be55bb83668f7acd4aafa429b0ce45
alejopua <alejopua@gmail.com>
2023-12-01 20:41:24 -0500

    [add](package): Install class validator

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import './style.css'
 import typescriptLogo from './typescript.svg'
 import viteLogo from '/vite.svg'
 import { setupCounter } from './counter.ts'
-import './course/app/02-main.ts'
+import './course/generics/02-base-http.services.ts'
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
   <div>
